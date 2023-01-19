e468a62d172b4172dbba9dd3f5e39763447cbe65
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-01-19 23:10:01 +0100

    Control del tiempo en 1 s

--- main.ts
+++ main.ts
@@ -4,14 +4,15 @@ function apagado () {
     pins.digitalWritePin(DigitalPin.P4, 0)
 }
 led.enable(false)
+let ts = 1.1
 basic.forever(function () {
     apagado()
     pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(200)
+    basic.pause(ts * 1000)
     apagado()
     pins.digitalWritePin(DigitalPin.P3, 1)
-    basic.pause(200)
+    basic.pause(ts * 1000)
     apagado()
     pins.digitalWritePin(DigitalPin.P4, 1)
-    basic.pause(200)
+    basic.pause(ts * 1000)
 })
