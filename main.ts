c5d4cb29cbf558c5ae02e965722139b610bcdd49
Aurelio Gallardo Rodríguez <aurelio@seritium.es>
2023-01-19 23:17:11 +0100

    Variación del semáforo con parpadeo

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+function Parpadeo (tempo: number, num: number) {
+    for (let index = 0; index < num; index++) {
+        apagado()
+        pins.digitalWritePin(DigitalPin.P3, 1)
+        basic.pause(tempo * 1000)
+        pins.digitalWritePin(DigitalPin.P3, 0)
+        basic.pause(tempo * 1000)
+    }
+}
 function apagado () {
     pins.digitalWritePin(DigitalPin.P2, 0)
     pins.digitalWritePin(DigitalPin.P3, 0)
@@ -5,13 +14,13 @@ function apagado () {
 }
 led.enable(false)
 let ts = 1.1
+let cadencia = 0.3
+let numero_parpadeos = 4
 basic.forever(function () {
     apagado()
     pins.digitalWritePin(DigitalPin.P2, 1)
     basic.pause(ts * 1000)
-    apagado()
-    pins.digitalWritePin(DigitalPin.P3, 1)
-    basic.pause(ts * 1000)
+    Parpadeo(cadencia, numero_parpadeos)
     apagado()
     pins.digitalWritePin(DigitalPin.P4, 1)
     basic.pause(ts * 1000)
